285f24b2af26545a8032a1bea409fadc7fece7c7
Yanick de Pater <yanick53@live.nl>
2021-11-18 14:39:38 +0100

    change var name

--- shodan_automator.py
+++ shodan_automator.py
@@ -3,8 +3,9 @@ from shodan.exception import APIError
 from argparse import ArgumentParser
 import logging
 
+__version__ = 0.1
 
-ascii = '''
+banner = f'''
 
 
    _____ __              __            ___         __                        __            
@@ -13,11 +14,12 @@ ascii = '''
  ___/ / / / / /_/ / /_/ / /_/ / / / / ___ / /_/ / /_/ /_/ / / / / / / /_/ / /_/ /_/ / /    
 /____/_/ /_/\____/\__,_/\__,_/_/ /_/_/  |_\__,_/\__/\____/_/ /_/ /_/\__,_/\__/\____/_/     
                                                                                            
-
+Version: {__version__}
 
 '''
 
-logFormatter = logging.Formatter("[%(levelname)s] - [%(asctime)s]: %(message)s")
+logFormatter = logging.Formatter(
+    "[%(levelname)s] - [%(asctime)s]: %(message)s")
 rootLogger = logging.getLogger()
 rootLogger.level = logging.INFO
 
@@ -43,31 +45,31 @@ class ShodanHostResult:
         self._ip = ip
         self._ports = ports
         self._hostnames = hostnames
-    
+
     @property
     def ip(self) -> str:
         return self._ip
-    
+
     @property
     def ports(self) -> list:
         return self._ports
-    
+
     @property
     def hostnames(self) -> list:
         return self._hostnames
-    
+
     @ip.setter
     def ip(self, ip) -> None:
         if not isinstance(ip, str):
             raise ValueError("IP must be of type string")
         self._ip = ip
-    
+
     @ports.setter
     def port(self, ports) -> None:
         if not isinstance(ports, list):
             raise ValueError("Ports must be of type list containing dicts")
         self._ports = ports
-    
+
     @hostnames.setter
     def hostnames(self, hostnames) -> None:
         if not isinstance(hostnames, list):
@@ -86,23 +88,23 @@ class ShodanScanner:
     @property
     def api_key(self) -> str:
         return self._api_key
-    
+
     @property
     def hosts(self) -> list:
         return self._hosts
-    
+
     @api_key.setter
     def api_key(self, api_key) -> None:
         if not isinstance(api_key, str):
             raise ValueError("The API key must be a string.")
         self._api_key = api_key
-    
+
     @hosts.setter
     def hosts(self, hosts):
         if not isinstance(hosts, list):
             raise ValueError("The hosts input must be a list.")
         self._hosts = hosts
-        
+
     def __repr__(self) -> str:
         return f"Scanner object: api_key: {self.api_key}, hosts: {self._hosts}"
 
@@ -114,33 +116,50 @@ class ShodanScanner:
             try:
                 result = api.host(h)
             except APIError as error_message:
-                rootLogger.error(f"API error occured for {h}, reason: {error_message}")
+                rootLogger.error(
+                    f"API error occured for {h}, reason: {error_message}")
                 continue
 
             ports = []
             for data_item in result['data']:
-                p = {"portnumber": data_item['port'], "transport": data_item['transport']}
+                p = {
+                    "portnumber": data_item['port'],
+                    "transport": data_item['transport']
+                }
                 ports.append(p)
             try:
-                shodan_hosts.append(ShodanHostResult(result["ip_str"], ports, result['hostnames']))
+                shodan_hosts.append(
+                    ShodanHostResult(result["ip_str"], ports,
+                                     result['hostnames']))
             except ValueError as v:
-                rootLogger.error(f"ValueError occured while creating new host result: {v}")
+                rootLogger.error(
+                    f"ValueError occured while creating new host result: {v}")
 
         return shodan_hosts
 
 
 if __name__ == "__main__":
-    print(ascii)
-    parser = ArgumentParser(description="ShodanAutomator automates the search of hosts from an input list. More to come.")
-    parser.add_argument("API_KEY", type=str, help="Submit your Shodan API key. This is required.")
+    print(banner)
+    parser = ArgumentParser(
+        description=
+        "ShodanAutomator automates the search of hosts from an input list. More to come."
+    )
+    parser.add_argument("API_KEY",
+                        type=str,
+                        help="Submit your Shodan API key. This is required.")
     parser.add_argument("--host", type=str, help="Input a single host")
-    parser.add_argument("--host-file", type=str, help="Input a file with hosts. Each on a new line.")
-    parser.add_argument("--verbose", "-v", action="store_true", help="Enable verbose ouptput (debug logging)")
+    parser.add_argument("--host-file",
+                        type=str,
+                        help="Input a file with hosts. Each on a new line.")
+    parser.add_argument("--verbose",
+                        "-v",
+                        action="store_true",
+                        help="Enable verbose ouptput (debug logging)")
     args = parser.parse_args()
 
     if not args.host and not args.host_file:
         raise MissingHostOrHosts()
-    
+
     if args.verbose:
         rootLogger.level = logging.DEBUG
 
@@ -154,11 +173,10 @@ if __name__ == "__main__":
             for line in infile:
                 line = line.strip()
                 _hosts.append(line)
-    
+
     scanner = ShodanScanner(api_key=args.API_KEY, hosts=_hosts)
     rootLogger.info("Scanner configured! Staring scan...")
     output = scanner.scan_hosts()
     for r in output:
         rootLogger.info(f"[*] - {r}")
     rootLogger.info("Scanner finished.")
-
